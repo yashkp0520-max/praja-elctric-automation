01e8a3a73036da5f92bcff1da7e69b6626955d90
yashkp0520-max <yashkp0520@gmail.com>
2026-06-08 13:19:11 +0530

    Update vite.config.js

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5174,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://praja-elctric-automation-backend.onrender.com',
         changeOrigin: true,
       },
     },
